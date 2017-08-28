63020a60bc733b417a70378b45fe22a01262891e
Takashi Okamoto <toraneko@gmail.com>
2017-08-28 09:35:20 +0900

    Add speech_recognizer.sh

---
+++ python/speech_recognizer.py
@@ -0,0 +1,38 @@
+﻿import sys
+import os
+import json
+import urllib.parse
+import urllib.request
+
+apikey = 'AIzaSyB4HQqiZ3uuVyhHTOoITpydT8lVzE4DBZg'
+endpoint = 'http://www.google.com/speech-api/v2/recognize'
+query_string = {'output': 'json', 'lang': 'en-US', 'key': apikey}
+
+url = '{0}?{1}'.format(endpoint, urllib.parse.urlencode(query_string))
+
+headers = {'Content-Type': 'audio/l16; rate=16000'}
+voice_data = open(sys.argv[1], 'rb').read()
+
+request = urllib.request.Request(url, data=voice_data, headers=headers)
+response = urllib.request.urlopen(request).read()
+str=response.decode('utf8')
+
+# print all json
+# print (str)
+
+# extract first transcript
+header='"transcript":"'
+spos = str.find(header)
+epos = str.find('"',len(header)+spos+1)
+transcript = str[spos+len(header):epos]
+print (transcript)
+
+#for line in response.decode('utf-8').split():
+#    if not line:
+#        continue
+#    else:
+#        res = json.loads(line)
+#        if res['result'] == []:
+#            continue
+#        else:
+#            print(res)
--- speech_recognizer.sh
+++ speech_recognizer.sh
@@ -1,8 +1,21 @@
 
+read -p "Push enter to record voice:" yn
+
 /c/Program\ Files\ \(x86\)/sox-14-4-2/sox.exe -t waveaudio --encoding signed-integer --bits 16 --channels 1 --rate 16000 -d $1&
 
 PID=$!
 
-sleep 5
+echo "Please ask a question. Now recording your speech."
+read -p "Push enter when finish the question:" yn
 
 kill $PID
+
+echo "Now recognizing speech..."
+TRANSCRIPT=$(/c/Program\ Files/LibreOffice\ 5/program/python.exe python/speech_recognizer.py $1)
+ 
+echo "recognition result: " ${TRANSCRIPT}
+read -p "Push enter to estimate answer:" yn
+ARY=($(java -classpath "lib/lucene-analyzers-3.6.2.jar;lib/lucene-spellchecker-3.6.2.jar;lib/lucene-core-3.6.2.jar;build/classes/java/main;." Matcher  "${TRANSCRIPT}"))
+
+read -p "Push enter to display:" yn
+winbin/ForHackathon.exe ${ARY[0]} ${ARY[1]} ${ARY[2]} ${ARY[3]}
